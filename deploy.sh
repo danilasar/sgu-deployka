ab30d078f82abc3116cce95a8d3765c3cfb73726
danilasar <danila.sar@yandex.ru>
2025-02-06 00:39:31 +0400

    refactor: split into functions by features

--- deploy.sh
+++ deploy.sh
@@ -13,32 +13,35 @@ heading() {
 	actionid++
 }
 
-heading "Получение устройства и проверка"
+check_device() {
+	heading "Получение устройства и проверка"
+	[ -b "$device" ] || { echo "Устройство $device не найдено"; exit 1; }
+	echo "Устройство $device обнаружено, начинаю развёртывание системы."
+}
 
-[ -b "$device" ] || { echo "Устройство $device не найдено"; exit 1; }
-echo "Устройство $device обнаружено, начинаю развёртывание системы."
+make_gpt() {
+	heading "Создание разделов GPT с помощью sgdisk"
+	echo "Удаляю таблицу разделов"
+	sgdisk --zap-all "$device"
 
-heading "Создание разделов GPT с помощью sgdisk"
-echo "Удаляю таблицу разделов"
-sgdisk --zap-all "$device"
+	# Типы разделов:
+	# EF00 - EFI
+	# 0C01 - Microsoft Reserved
+	# 2700 - Windows Recovery
+	# 0700 - NTFS
+	# 8300 - Linux
+	# 8200 - Swap
 
-# Типы разделов:
-# EF00 - EFI
-# 0C01 - Microsoft Reserved
-# 2700 - Windows Recovery
-# 0700 - NTFS
-# 8300 - Linux
-# 8200 - Swap
-
-echo "Создаю новую таблицу разделов"
-sgdisk --align-end=optimal \
-  --new=1:0:+128M   --typecode=1:0C01  --change-name=1:"Microsoft Reserved" \
-  --new=2:0:+529M   --typecode=2:2700  --change-name=2:"Recovery" \
-  --new=3:0:+100M   --typecode=3:EF00  --change-name=3:"EFI" \
-  --new=4:0:+50%    --typecode=4:0700  --change-name=4:"Windows" \
-  --new=5:0:-8G     --typecode=5:8300  --change-name=5:"Linux" \
-  --new=6:0:0       --typecode=6:8200  --change-name=6:"Swap" \
-  "$device"
+	echo "Создаю новую таблицу разделов"
+	sgdisk --align-end=optimal \
+	  --new=1:0:+128M   --typecode=1:0C01  --change-name=1:"Microsoft Reserved" \
+	  --new=2:0:+529M   --typecode=2:2700  --change-name=2:"Recovery" \
+	  --new=3:0:+100M   --typecode=3:EF00  --change-name=3:"EFI" \
+	  --new=4:0:+50%    --typecode=4:0700  --change-name=4:"Windows" \
+  	--new=5:0:-8G     --typecode=5:8300  --change-name=5:"Linux" \
+	  --new=6:0:0       --typecode=6:8200  --change-name=6:"Swap" \
+	  "$device"
+}
 
 # ------------------------------------------------------------
 # Функция копирования образов (локально/FTP/SSH)
@@ -63,93 +66,107 @@ copy_partition() {
   esac
 }
 
-heading "Копирование образов"
-echo "ms_reserved.img..."
-copy_partition 1 "$device/ms_reserved.img"
-echo "recovery.img..."
-copy_partition 2 "$device/recovery.img"
-echo "efi.img..."
-copy_partition 3 "$device/efi.img"
-echo "windows.img..."
-copy_partition 4 "$device/windows.img"
-echo "linux.img..."
-copy_partition 5 "$device/linux.img"
-
-heading "Обновление fstab"
-linux_part="${device}5"
-efi_part="${device}3"
-swap_part="${device}6"
-
-echo "Монтирую разделы"
-mount "$linux_part" /mnt
-mount "$efi_part" /mnt/boot/efi
-
-echo "Генерую новый fstab"
-{
-  echo "# /etc/fstab"
-  echo "UUID=$(sudo blkid -s UUID -o value "$efi_part")  /boot/efi  vfat  umask=0077  0  1"
-  echo "UUID=$(sudo blkid -s UUID -o value "$linux_part")  /  ext4  defaults  0  1"
-  echo "UUID=$(sudo blkid -s UUID -o value "$swap_part")  none  swap  sw  0  0"
-} | tee /mnt/etc/fstab
-
-echo "Размонтирую разделы"
-umount "$efi_part"
-umount "$linux_part"
-
-heading "Добавление загрузчика Альт Линукса в EFI"
-
-efi_mount="/mnt"
-shim_path="$efi_mount/EFI/altlinux/shimx.64.efi"
-echo "Монтирую EFI-раздел в $efi_mount"
-mount "${device}3" $efi_mount
-
-echo "Проверяю существование shim"
-[ -f "$shim_path" ] || {
-	echo "$shim_path не найден"
-	exit 2
+copy_images() {
+	heading "Копирование образов"
+	echo "ms_reserved.img..."
+	copy_partition 1 "$device/ms_reserved.img"
+	echo "recovery.img..."
+	copy_partition 2 "$device/recovery.img"
+	echo "efi.img..."
+	copy_partition 3 "$device/efi.img"
+	echo "windows.img..."
+	copy_partition 4 "$device/windows.img"
+	echo "linux.img..."
+	copy_partition 5 "$device/linux.img"
 }
 
-echo "Проверяю существование загрузочной записи"
-existing_entry=$(efibootmgr -v | grep -i "Alt Linux" | grep -Eo 'Boot[0-9A-F]{4}')
+update_fstab() {
+	heading "Обновление fstab"
+	linux_part="${device}5"
+	efi_part="${device}3"
+	swap_part="${device}6"
 
-if [ -n "$existing_entry" ]; then
-	echo "Найдена загрузочная запись: $existing_entry"
-	bootnum=${existing_entry}
-else
-	echo "Не нашёл, создаю новую загрузочную запись"
-	new_entry=$(efibootmgr \
-		--create \
-		--disk "$device" \
-		--part 3 \
-		--loader "\\EFI\\altlinux\\shimx64.efi" \
-		--label "Alt Linux" \
-		--verbose 2>&1 | grep -Eo 'Boot[0-9A-F]{4}' \
-	)
+	echo "Монтирую разделы"
+	mount "$linux_part" /mnt
+	mount "$efi_part" /mnt/boot/efi
 
-	[ -n "$new_entry" ] || {
-		echo "Не удалось создать запись EFI"
-		exit 3
+	echo "Генерую новый fstab"
+	{
+  	echo "# /etc/fstab"
+	  echo "UUID=$(sudo blkid -s UUID -o value "$efi_part")  /boot/efi  vfat  umask=0077  0  1"
+  	echo "UUID=$(sudo blkid -s UUID -o value "$linux_part")  /  ext4  defaults  0  1"
+	  echo "UUID=$(sudo blkid -s UUID -o value "$swap_part")  none  swap  sw  0  0"
+	} | tee /mnt/etc/fstab
+
+	echo "Размонтирую разделы"
+	umount "$efi_part"
+	umount "$linux_part"
+}
+
+update_efi() {
+	heading "Добавление загрузчика Альт Линукса в EFI"
+	
+	efi_mount="/mnt"
+	shim_path="$efi_mount/EFI/altlinux/shimx.64.efi"
+	echo "Монтирую EFI-раздел в $efi_mount"
+	mount "${device}3" $efi_mount
+	
+	echo "Проверяю существование shim"
+	[ -f "$shim_path" ] || {
+		echo "$shim_path не найден"
+		exit 2
 	}
+	
+	echo "Проверяю существование загрузочной записи"
+	existing_entry=$(efibootmgr -v | grep -i "Alt Linux" | grep -Eo 'Boot[0-9A-F]{4}')
+	
+	if [ -n "$existing_entry" ]; then
+		echo "Найдена загрузочная запись: $existing_entry"
+		bootnum=${existing_entry}
+	else
+		echo "Не нашёл, создаю новую загрузочную запись"
+		new_entry=$(efibootmgr \
+			--create \
+			--disk "$device" \
+			--part 3 \
+			--loader "\\EFI\\altlinux\\shimx64.efi" \
+			--label "Alt Linux" \
+			--verbose 2>&1 | grep -Eo 'Boot[0-9A-F]{4}' \
+		)
+	
+		[ -n "$new_entry" ] || {
+			echo "Не удалось создать запись EFI"
+			exit 3
+		}
+	
+		bootnum=${new_entry#Boot}
+	fi
+	
+	echo "Размонтирую EFI-раздел"
+	umount $efi_mount
+	
+	echo "Получаю порядок загрузки"
+	current_order=$(efibootmgr  | grep "BootOrder:" | cut -d: -f2 | tr -d ' ')
+	
+	echo "Формирую новый порядок загрузки"
+	new_order="$bootnum,$(echo "$current_order" | \
+		tr ',' '\n' | \
+		grep -vx "$bootnum" | \
+		tr '\n' ',' | \
+		sed 's/,$//' \
+	)"
+	
+	echo "Сохраняю новый порядок"
+	efibootmgr --bootorder "$new_order"
+	
+	echo "Текущая конфигурация загрузки:"
+	efibootmgr | grep -v "BootOrder:"
+}
 
-	bootnum=${new_entry#Boot}
-fi
 
-echo "Размонтирую EFI-раздел"
-umount $efi_mount
-
-echo "Получаю порядок загрузки"
-current_order=$(efibootmgr  | grep "BootOrder:" | cut -d: -f2 | tr -d ' ')
-
-echo "Формирую новый порядок загрузки"
-new_order="$bootnum,$(echo "$current_order" | \
-	tr ',' '\n' | \
-	grep -vx "$bootnum" | \
-	tr '\n' ',' | \
-	sed 's/,$//' \
-)"
-
-echo "Сохраняю новый порядок"
-efibootmgr --bootorder "$new_order"
-
-echo "Текущая конфигурация загрузки:"
-efibootmgr | grep -v "BootOrder:"
+check_device
+make_gpt
+copy_partition
+copy_images
+update_fstab
+update_efi
